91f9d7ac778d907681ec9a985ff69b4aac428dc3
chuyenvien986-dot <chuyenvien986@gmail.com>
2026-09-12 17:23:48 +0700

    Fix missing closing brace and semicolon in config.js
    Added missing closing brace and semicolon at the end of the module exports.

--- build/config.js
+++ build/config.js
@@ -1,23 +1,21 @@
 "use strict";
 const path = require("path");
-
 const A = (...p) => path.join(__dirname, "assets", ...p);
 
 module.exports = {
-    
     BRAND_NAME:  "Drilper Gangs",
     BRAND_OWNER: "NGUYỄN THÀNH KHANG",
     BRAND_TAG:   "NGUYỄN THÀNH KHANG DRILPER",
     FOOTER:      "Make by NGUYỄN THÀNH KHANG DRILPER",
 
-    IMG_JOIN_PROMPT:  A("join_prompt.jpg"),
-    IMG_DM_GUIDE:     A("dm_guide.jpg"),
-    IMG_TOKENS_OK:    A("tokens_ok.jpg"),
-    IMG_GUILD_OK:     A("guild_ok.jpg"),
-    IMG_JOIN_START:   A("join_start.jpg"),
-    IMG_PROGRESS:     A("progress.gif"),
-    IMG_DONE:         A("done.jpg"),
-    IMG_ERROR:        A("error.jpg"),
+    IMG_JOIN_PROMPT: A("join_prompt.jpg"),
+    IMG_DM_GUIDE:    A("dm_guide.jpg"),
+    IMG_TOKENS_OK:   A("tokens_ok.jpg"),
+    IMG_GUILD_OK:    A("guild_ok.jpg"),
+    IMG_JOIN_START:  A("join_start.jpg"),
+    IMG_PROGRESS:    A("progress.gif"),
+    IMG_DONE:        A("done.jpg"),
+    IMG_ERROR:       A("error.jpg"),
 
     COLOR_MAIN: "Blue",
     COLOR_OK:   "Green",
@@ -33,4 +31,4 @@ module.exports = {
     PROGRESS_THROTTLE_MS:   1200,
 
     PROXY: null,
-}; // <--- Đã thêm dấu đóng ngoặc nhọn và dấu chấm phẩy ở đây
+};
