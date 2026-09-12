0e849f11087cbb0404e8fae5fc33b652ef3091d5
chuyenvien986-dot <chuyenvien986@gmail.com>
2026-09-12 17:13:41 +0700

    Update config.js

--- build/config.js
+++ build/config.js
@@ -5,16 +5,11 @@ const A = (...p) => path.join(__dirname, "assets", ...p);
 
 module.exports = {
     
-
     BRAND_NAME:  "Drilper Gangs",
     BRAND_OWNER: "NGUYỄN THÀNH KHANG",
     BRAND_TAG:   "NGUYỄN THÀNH KHANG DRILPER",
     FOOTER:      "Make by NGUYỄN THÀNH KHANG DRILPER",
 
-    
-    
-    
-    
     IMG_JOIN_PROMPT:  A("join_prompt.jpg"),
     IMG_DM_GUIDE:     A("dm_guide.jpg"),
     IMG_TOKENS_OK:    A("tokens_ok.jpg"),
@@ -24,17 +19,11 @@ module.exports = {
     IMG_DONE:         A("done.jpg"),
     IMG_ERROR:        A("error.jpg"),
 
-    
-    
-    
     COLOR_MAIN: "Blue",
     COLOR_OK:   "Green",
     COLOR_WARN: "Yellow",
     COLOR_FAIL: "Red",
 
-    
-    
-    
     PREFIX:  "!",
     COMMAND: "join",
 
@@ -44,3 +33,4 @@ module.exports = {
     PROGRESS_THROTTLE_MS:   1200,
 
     PROXY: null,
+}; // <--- Đã thêm dấu đóng ngoặc nhọn và dấu chấm phẩy ở đây
